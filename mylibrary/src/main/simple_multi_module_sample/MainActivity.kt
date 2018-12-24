6f3f6153a2274a86a9b8525ca6d578c43da59da6
yoshito iwaisako <iwaisako@andfactory.co.jp>
2018-12-24 15:28:54 +0900

    add mylibrary module

---
+++ mylibrary/src/main/simple_multi_module_sample/MainActivity.kt
@@ -0,0 +1,12 @@
+package jp.yiwaisako.simple_multi_module_sample
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
